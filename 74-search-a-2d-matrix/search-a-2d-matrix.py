c0501e5c10cc8ec3ffb4e8084a6781f609943954
jaltareyr <91781874+jaltareyr@users.noreply.github.com>
2024-10-22 19:18:47 -0400

    Time: 3 ms (95.24%) | Memory: 11.7 MB (99.78%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m, n = len(matrix), len(matrix[0])
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == target:
+                    return True
+        return False
