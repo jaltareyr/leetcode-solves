37794f4dd0004ff2c5b91137117870487d447da3
jaltareyr <91781874+jaltareyr@users.noreply.github.com>
2024-10-24 17:42:54 -0400

    Time: 276 ms (42.08%) | Memory: 13 MB (79.08%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -5,34 +5,11 @@ class Solution(object):
         :type word2: str
         :rtype: bool
         """
-        
-        flag1 = False
-        flag2 = False
 
-        if set(word1) == set(word2):
-            flag1 = True
-        
-        dict1 = {}
-        dict2 = {}
-        for ch in word1:
-            if ch in dict1:
-                dict1[ch]+=1
-            else:
-                dict1[ch]=1
-
-        for ch in word2:
-            if ch in dict2:
-                dict2[ch]+=1
-            else:
-                dict2[ch]=1
-
-        print(dict1)
-        print(dict2)
-        
-        if sorted(dict1.values())==sorted(dict2.values()):
-            flag2=True
-        
-        if flag1 and flag2:
-            return True
-        else:
+        if set(word1) != set(word2):
             return False
+        
+        dict1 = Counter(word1)
+        dict2 = Counter(word2)
+        
+        return sorted(dict1.values())==sorted(dict2.values())
