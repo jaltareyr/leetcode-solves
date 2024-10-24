0c4eb1d620dab0e2d038287863fd1bcd5505e92e
jaltareyr <91781874+jaltareyr@users.noreply.github.com>
2024-10-24 17:37:37 -0400

    Time: 345 ms (26.51%) | Memory: 13 MB (79.08%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -5,36 +5,23 @@ class Solution(object):
         :type word2: str
         :rtype: bool
         """
-        
-        flag1 = False
-        flag2 = False
+        from collections import Counter
 
-        print(set(list(word1)), set(list(word2)))
-
-        if set(list(word1)) ==set(list(word2)):
-            flag1 = True
+        freq1 = Counter(word1)
+        freq2 = Counter(word2)
+        flg1 = False
+        flg2 = False
+        if set(freq1.keys()) == set(freq2.keys()):
+            flg1 = True
         
-        dict1 = {}
-        dict2 = {}
-        for ch in word1:
-            if ch in dict1:
-                dict1[ch]+=1
-            else:
-                dict1[ch]=1
+        if Counter(freq1.values()) == Counter(freq2.values()):
+            flg2 = True
 
-        for ch in word2:
-            if ch in dict2:
-                dict2[ch]+=1
-            else:
-                dict2[ch]=1
-
-        print(dict1)
-        print(dict2)
+        print(flg1, flg2)
         
-        if sorted(dict1.values())==sorted(dict2.values()):
-            flag2=True
-        
-        if flag1 and flag2:
+        if flg1 and flg2:
             return True
         else:
             return False
+        
+        
