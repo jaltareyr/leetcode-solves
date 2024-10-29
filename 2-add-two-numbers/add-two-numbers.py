c3e799e7076479e2eeff2be31e337e992c099e7a
jaltareyr <91781874+jaltareyr@users.noreply.github.com>
2024-10-28 20:27:40 -0400

    Time: 8 ms (59.41%) | Memory: 11.8 MB (9.00%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -11,23 +11,30 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         
-        result = ListNode()
-        current = result
+        head = l1  # Use `l1` as the result list
+        carry = 0  # Initialize carry
 
-        tenth = 0
-
-        while l1 or l2 or tenth:
+        # Loop through both lists
+        prev = None
+        while l1 or l2 or carry:
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
-
-            addition = val1 + val2 + tenth
-            tenth = addition//10
-            current.next = ListNode(addition%10)
-
-            current = current.next
-
+            
+            # Calculate the sum and update carry
+            addition = val1 + val2 + carry
+            carry = addition // 10
+            
+            # Update l1 node's value or create new node if needed
             if l1:
+                l1.val = addition % 10
+                prev = l1
                 l1 = l1.next
+            else:
+                prev.next = ListNode(addition % 10)
+                prev = prev.next
+            
+            # Move l2 pointer
             if l2:
                 l2 = l2.next
-        return result.next
+        
+        return head
