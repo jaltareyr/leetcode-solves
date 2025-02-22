9b466e595dccac0fbc50549ec85d8fb462d8a999
Yashodhan Jaltare <91781874+jaltareyr@users.noreply.github.com>
2025-02-21 22:10:00 -0500

    Time: 938 ms (5.16%) | Memory: 12.9 MB (78.40%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -4,18 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: List[str]
         """
-        def dfs(s, temp, start):
+        def dfs(s, result, temp, start):
             if len(temp) == len_max:
-                result.append(''.join(temp[:]))
+                result.append(temp)
                 return
             for i in range(start, len(s)):
                 if s[i].isdigit():
-                    dfs(s, temp + [s[i]], i + 1)
+                    dfs(s, result, temp + s[i], i + 1)
                 else:
-                    dfs(s, temp + [s[i].lower()], i + 1)
-                    dfs(s, temp + [s[i].upper()], i + 1)
+                    dfs(s, result, temp + s[i].lower(), i + 1)
+                    dfs(s, result, temp + s[i].upper(), i + 1)
         
         result = []
         len_max = len(s)
-        dfs(s, [], 0)
+        dfs(s, result,"", 0)
         return result
